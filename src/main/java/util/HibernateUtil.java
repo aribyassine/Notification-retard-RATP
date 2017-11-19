92292895290bd59d1fa6507cf1e7b755b2ea7d0f
yar <yassine.arib@lesfurets.com>
2017-11-19 01:05:57 +0100

    env heroku

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -6,12 +6,31 @@ import org.hibernate.cfg.*;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
+import java.net.URI;
+
 public class HibernateUtil {
 	public static final SessionFactory sessionFactory;
 
 	static {
 		try {
 			Configuration configuration = new Configuration().configure();
+            final String DATABASE_URL = System.getenv("DATABASE_URL");
+            if (DATABASE_URL != null && !DATABASE_URL.isEmpty()) {
+
+                URI dbUri = new URI(DATABASE_URL);
+                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+                String username = dbUri.getUserInfo().split(":")[0];
+                String password = dbUri.getUserInfo().split(":")[1];
+                System.out.println(dbUrl);
+                configuration.setProperty("hibernate.connection.username", username);
+                configuration.setProperty("hibernate.connection.password", password);
+                configuration.setProperty("hibernate.connection.url", dbUrl);
+                configuration.setProperty("connection.username", username);
+                configuration.setProperty("connection.password", password);
+                configuration.setProperty("connection.url", dbUrl);
+
+                configuration.getProperties().list(System.out);
+            }
 
 			ServiceRegistryBuilder registry = new ServiceRegistryBuilder();
 			registry.applySettings(configuration.getProperties());
