946b75003456d218893ff8af4a9aeb518b85b6ca
mohamedKassar <ktarek1994@gmail.com>
2017-11-21 16:12:31 +0100

    fix notifications order

--- src/main/java/controllers/servlets/UserNotifications.java
+++ src/main/java/controllers/servlets/UserNotifications.java
@@ -1,9 +1,10 @@
 
-
 package controllers.servlets;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -15,25 +16,25 @@ import javax.servlet.http.HttpSession;
 
 import controllers.UserNotificationsController;
 import controllers.exceptions.DataException;
+import model.entities.Comment;
 import model.entities.Notification;
 
-
-@WebServlet(name = "usernotifications", urlPatterns = {"/usernotifications"})
+@WebServlet(name = "usernotifications", urlPatterns = { "/usernotifications" })
 public class UserNotifications extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private UserNotificationsController nct = new UserNotificationsController();
-	
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
 			String login = (String) req.getSession().getAttribute("username");
-			List<Notification> nots = nct.getUserNotifications(login);
+			List<Notification> nots = nct.getUserNotifications(login).stream()
+					.sorted((n1, n2) -> n2.getDate().compareTo(n1.getDate())).collect(Collectors.toList());
 			req.setAttribute("notifications", nots);
-			req.getRequestDispatcher("/jsp/userNotifications.jsp").forward(req,resp);
+			req.getRequestDispatcher("/jsp/userNotifications.jsp").forward(req, resp);
 			resp.setStatus(HttpServletResponse.SC_OK);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		}
 
@@ -41,8 +42,6 @@ public class UserNotifications extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
 
 	}
 
